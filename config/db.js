aa327860ec4056e2e0afc10c873678b377f86f59
muxriddin-web <otkirovmuxriddin01@gmail.com>
2026-03-03 22:19:27 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
 //  3. config/db.js — MongoDB ulanish
 // config/db.js
+// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`✅ MongoDB ulandi: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB ulanishda xatolik:", error.message);
-    process.exit(1); // jarayonni to‘xtatadi
+    process.exit(1);
   }
 };
 
